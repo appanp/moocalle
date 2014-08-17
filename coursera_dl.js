3aba2d1da0f79b092f7d0af1fe70ead59b5d27fc
Appan Ponnappan <appan_ponnappan@yahoo.com.sg>
2014-08-17 22:54:17 +0530

    Fixes #2

--- coursera_dl.js
+++ coursera_dl.js
@@ -32,13 +32,22 @@ var api_pg_url = 'https://class.coursera.org/'+class_name+'/api/forum/forums/0/t
 var api_post_url_pfx = 'https://class.coursera.org/'+class_name+'/api/forum/threads/';
 var delay = process.argv[3]; //7 sec delay between requests
 var strt_page_id = 1;
+var class_dir = '../'+class_name
+var class_posts_dir = '../'+class_name+'/posts';
+var class_lists_dir = '../'+class_name+'/lists';
+
 if (process.argv.length >=5) {
     strt_page_id = process.argv[4];
 }
+
+function get_list_urls() {
+
+}
+
 function get_posts_in(urls,delay) {
   url = urls.shift();
   post_id = url.substr(url.lastIndexOf('=')+1);
-  op_file = '../'+class_name+'/posts/'+post_id+'.json';
+  op_file = class_posts_dir+'/'+post_id+'.json';
   post_url = api_post_url_pfx + post_id +'?sort=null';
   request({
   	url: post_url,
@@ -85,8 +94,8 @@ function get_posts_list(delay, pg_id) {
   if ( typeof get_posts_list.page_id == 'undefined') {
     get_posts_list.page_id = Number(pg_id);
     get_posts_list.timestamp = Date.now();
-		get_posts_list.max_page_id = 500;
-		get_posts_list.fetched_posts = 0;
+	get_posts_list.max_page_id = 500;
+	get_posts_list.fetched_posts = 0;
 	}
   else {
     console.log("...Time to fetch "+get_posts_list.fetched_posts+" posts: " + (Date.now() - get_posts_list.timestamp));
@@ -94,8 +103,25 @@ function get_posts_list(delay, pg_id) {
   	get_posts_list.page_id += 1;
 	}
 
-  op_file = '../'+class_name+'/lists/'+get_posts_list.page_id+'.json';
+  op_file = class_lists_dir+'/'+get_posts_list.page_id+'.json';
   if ( get_posts_list.page_id <= get_posts_list.max_page_id ) {
+   var urls = [];
+   if (typeof pg_id != 'undefined') {
+        var json_file = class_lists_dir+'/'+pg_id+'.json';
+        var lst_json_str = fs.readFileSync(json_file);
+        var lst_json_obj = JSON.parse(lst_json_str);
+		get_posts_list.max_page_id = lst_json_obj['max_pages'];
+		//Get the list of post URLs & return the array of URLs
+		posts = lst_json_obj['threads']
+        list_files = fs.readdirSync(class_posts_dir);
+		for(var i=list_files.length % 25;i < posts.length;i++) {
+            urls.push(posts[i]['_link']);
+		}
+		get_posts_list.fetched_posts = urls.length;
+        console.log("...Resuming from post id: "+urls[0]);
+        get_posts_in(urls,delay);
+   }
+   else {
    	request({
   		url: api_pg_url+'&page='+get_posts_list.page_id,
   		//timeout: req_to,
@@ -106,7 +132,6 @@ function get_posts_list(delay, pg_id) {
   		},
   		function (err, rsp, body) {
       	  console.log('Fetching posts page: '+get_posts_list.page_id);
-	  	  var urls = [];
       	  if (!err && rsp.statusCode == 200) {
 			if (rsp.headers['content-encoding'] == 'gzip') {
 				zlib.gunzip(body, function(err2, dezipped) {
@@ -132,9 +157,10 @@ function get_posts_list(delay, pg_id) {
 			}
       	  }
           else {
-            console.log('...Error fetching posts page');
+            console.log('...Error fetching posts page, error is: '+rsp.statusCode);
           }
   		});
+       }
   }
   else {
       console.log("...Terminating since reached max: "+get_posts_list.page_id+"/"+get_posts_list.max_page_id);
@@ -144,18 +170,19 @@ function get_posts_list(delay, pg_id) {
 // START of the MAIN program
 // Do the initialization of creating required dirs & 
 // reading in the state stored from previous run
-var class_dir = '../'+class_name
-var class_posts_dir = '../'+class_name+'/posts';
-var class_lists_dir = '../'+class_name+'/lists';
-
 try {
     fs.mkdirSync(class_dir);
     fs.mkdirSync(class_posts_dir);
     fs.mkdirSync(class_lists_dir);
   } catch(e) {
-    if ( e.code != 'EEXIST' ) throw e;
+    if ( e.code == 'EEXIST' ) {
+        list_files = fs.readdirSync(class_lists_dir);
+        strt_page_id = list_files.length;
+        }
+    else throw e;
   }
 
 // Format the initial request and get the first page of posts
+console.log('Starting with list ID: '+strt_page_id);
 get_posts_list(delay,strt_page_id);
 
